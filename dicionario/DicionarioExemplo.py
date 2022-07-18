1804f22a23e6e351af2162ed7a73ce04e8036b8c
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-07-18 12:43:27 -0300

    dicionarios

---
+++ dicionario/DicionarioExemplo.py
@@ -0,0 +1,12 @@
+usuarios  = {}
+
+usuarios = {"chaves" : ["chaves do 8", "24/12/2017", "Recep_01"],
+            "kiko": ["Quico das flores","20/12/17", "Raiox_03"]}
+print(usuarios)
+
+usuarios["florinda"] = ["Dona Florinda", "24/12/201", "Raiox_01"]
+
+print(usuarios)
+
+print("####------####")
+print(usuarios.get("chaves"))
