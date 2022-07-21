660b98a4f9f54b71e783773746a18f90f24f6d1d
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-07-20 23:49:24 -0300

    FTP protocolos

---
+++ Cap7_Socket/Protocolo_FTP2.py
@@ -0,0 +1,11 @@
+from ftplib import *
+ftp_ativo=False
+ftp = FTP('ftp.ibge.gov.br')
+print(ftp.getwelcome())
+usuario=input("Digite o usuario: ")
+senha=input("Digite a senha: ")
+ftp.login(usuario, senha)
+print("Diretório atual de trabalho: ", ftp.pwd())
+ftp.cwd('etc')
+print("Diretório corrente: ", ftp.pwd())
+ftp.quit()
